c853630d4c04e5e9a5c103f83707a73feb9d7d71
cesar henrique <cesar_silk321@hotmail.com>
2022-03-29 00:05:52 -0300

    primeiro

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
-import { Item } from './item'
-import { ItemService } from './item.service'
+import {Item} from './item';
+import {ItemService} from './item.service';
+import {allowSleepAgain, keepAwake} from 'nativescript-insomnia';
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
 })
-export class ItemsComponent implements OnInit {
-  items: Array<Item>
+export class ItemsComponent implements OnInit, OnDestroy {
+  items: Array<Item>;
 
-  constructor(private itemService: ItemService) {}
+  constructor(private itemService: ItemService) {
+  }
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
+    keepAwake().then(() => console.log('Insomnia is now ON'));
+    this.items = this.itemService.getItems();
+  }
+
+  ngOnDestroy(): void {
+    allowSleepAgain().then(() => console.log('Insomnia is now OFF'));
   }
 }
